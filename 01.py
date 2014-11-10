dbc8e5cf6bb6d0ed9da4934f96bba99af5ad8435
yoshi <yoshi@yoshi-no-MacBook.local>
2014-11-10 19:24:52 +0900

    Base program No.2

--- 01.py
+++ 01.py
@@ -5,28 +5,45 @@ cmplist = []
 mylist = []
 def search(arg_list, x, y):
 	global c
+	global cmplist
 	c += 1
-	if x < 0 or y < 0 or x > len(arg_list[0]) or y > len(arg_list):
-		print "nil:" + str(c) + ":" + "x = " + str(x) + ": " + "y = " + str(y) + "\n"
+	print x
+	print str(y) + '\n'
+	if x < 0 or y < 0 or y >= len(arg_list) or x >= len(arg_list[0]) or cmplist[y][x] != ' ' or c > 30 :
+		print "nil:" + str(c) + ":" + "x = " + str(x) + ": " + "y = " + str(y)
+		print str(cmplist[y][x])
 		return 1
-	search(arg_list, x, y-1)
-	search(arg_list, x+1, y)
-	search(arg_list, x, y+1)
-	search(arg_list, x-1, y)
+
+	cmplist[y][x] = '*'
+	for tmp in cmplist :      
+		print tmp
+
+	print "hoge" + str(cmplist[y][x]) + "aaa"
+	
+	if cmplist[y][x-1] == ' ' :
+		search(arg_list, y, x-1)
+	if cmplist[y+1][x] == ' ' :
+		search(arg_list, y+1, x)
+	if cmplist[y][x+1] == ' ' :
+		search(arg_list, y, x+1)
+	if cmplist[y-1][x] == ' ' :
+		search(arg_list, y-1, x)
 
 for line in open('data.txt', 'r'):
 	line = line.rstrip()
-	mylist.append(line)
+	mylist.append(str(line))
 	#initialize output list
-	cmplist.append(' ' * len(line))
+	cmplist.append(map(str, str(' ' * len(line))))
 
 # input
 for x in mylist :      
 	print x;
 
+search(mylist, 0, 0)
+
 # output
 for x in cmplist :      
 	print x;
 
-#search(mylist, 0, 0)
+
 
