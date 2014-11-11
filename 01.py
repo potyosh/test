e5847497f678d0d5111b3ecb19a6e7678551dcb6
yoshi <yoshi@yoshi-no-MacBook.local>
2014-11-11 19:38:59 +0900

    experiment

--- 01.py
+++ 01.py
@@ -1,24 +1,27 @@
 #!/usr/bin/python
-c = 0
+#c = 0
 # + or - or / or *
 cmplist = []
 mylist = []
+color = '*'
 
 def search(arg_list, y, x):
-	global c
+	#global c
 	#global cmplist
-	c += 1
-	print y
-	print x
-	if x < 0 or y < 0 or y >= len(arg_list) or x >= len(arg_list[0]) or cmplist[y][x] != ' ' or c > 30 :
-		print "nil:" + str(c) + ":" + "y = " + str(y) + ": " + "x = " + str(x)
-		return 1
+	#c += 1
+	
+	#print y
+	#print x
+	#if x < 0 or y < 0 or y >= len(arg_list) or x >= len(arg_list[0]) or cmplist[y][x] != ' ' or c > 30 :
+	#	print "nil:" + str(c) + ":" + "y = " + str(y) + ": " + "x = " + str(x)
+	#	return 1
 
 	for tmp in cmplist :      
 		print tmp
 
-	print "hoge" + str(cmplist[y][x]) + "aaa"
-	cmplist[y][x] = '*'
+	#print "hoge" + str(cmplist[y][x]) + "aaa"
+	print "------------------------"
+	cmplist[y][x] = color
 	if y > 0 and cmplist[y-1][x] == ' ' and arg_list[y][x] == arg_list[y-1][x]:
 		search(arg_list, y-1, x)
 	if x+1 < len(arg_list[0]) and cmplist[y][x+1] == ' ' and arg_list[y][x] == arg_list[y][x+1]:
@@ -38,9 +41,19 @@ for line in open('data.txt', 'r'):
 for x in mylist :      
 	print x;
 
-#cmplist[0][0] = '*'
 search(mylist, 0, 0)
-
+color = '-'
+search(mylist, 0, 1)
+color = '/'
+search(mylist, 0, 2)
+color = '-'
+search(mylist, 0, 5)
+color = '/'
+search(mylist, 0, 9)
+color = '/'
+search(mylist, 3, 0)
+color = '+'
+search(mylist, 3, 2)
 # output
 for x in cmplist :      
 	print x;
