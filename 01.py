81363d75ec1c1dcabf38c0254239b48be340b5d2
yoshi <yoshi@yoshi-no-MacBook.local>
2014-11-14 16:53:48 +0900

    for backtrace

--- 01.py
+++ 01.py
@@ -1,52 +1,65 @@
 #!/usr/bin/python
 # + or - or / or *
-cmplist = []
-mylist = []
-colorlist = []
-poslist = []
 
-def setColor(arg_poslist, arg_color):
-	print arg_poslist
-	print arg_color
+class AllPosColor:
+	def __init__(self, arg_poslist, arg_availColList):
+		self.poslist = []
+		self.arg_availColList = []
+
+def setColor(arg_poslist, arg_color, arg_cmplist):
+	for pos in arg_poslist:
+		arg_cmplist[pos[0]][pos[1]] = arg_color
 
 def getAvailableColor(arg_colorlist, arg_poslist):
 	print "------------------------"
 	usedColor = []
+	ansColList = []
+	ansCol = '@'
 	for pos in arg_poslist:
 		#above
-		if pos[0]-1 > 0 and cmplist[pos[0]-1][pos[1]] != '@':
+		if pos[0]-1 > 0 and arg_colorlist[pos[0]-1][pos[1]] != '@':
 			if arg_colorlist[pos[0]-1][pos[1]] not in usedColor:
 				usedColor.append(arg_colorlist[pos[0]-1][pos[1]])
 		#right
-		if pos[1]+1 < len(arg_colorlist[0]) and cmplist[pos[0]][pos[1]+1] != '@':
+		if pos[1]+1 < len(arg_colorlist[0]) and arg_colorlist[pos[0]][pos[1]+1] != '@':
 			if arg_colorlist[pos[0]][pos[1]+1] not in usedColor:
 				usedColor.append(arg_colorlist[pos[0]][pos[1]+1])
 		#bottom
-		if pos[0]+1 < len(arg_colorlist) and cmplist[pos[0]+1][pos[1]] != '@':
+		if pos[0]+1 < len(arg_colorlist) and arg_colorlist[pos[0]+1][pos[1]] != '@':
 			if arg_colorlist[pos[0]+1][pos[1]] not in usedColor:
 				usedColor.append(arg_colorlist[pos[0]+1][pos[1]])
 		#left
-		if pos[1]-1 > 0 and cmplist[pos[0]][pos[1]-1] != '@':
+		if pos[1]-1 > 0 and arg_colorlist[pos[0]][pos[1]-1] != '@':
 			if arg_colorlist[pos[0]][pos[1]-1] not in usedColor:
 				usedColor.append(arg_colorlist[pos[0]][pos[1]-1])
 
 	if '+' not in usedColor:
-		answer = '+'
+		ansCol = '+'
 	elif '-' not in usedColor:
-		answer = '-'
+		ansCol = '-'
 	elif '/' not in usedColor:
-		answer = '/'
+		ansCol = '/'
 	elif '*' not in usedColor:
-		answer = '*'
+		ansCol = '*'
 
-	return answer
+	if '+' not in usedColor:
+		ansColList.append('+')
+	if '-' not in usedColor:
+		ansColList.append('-')
+	if '/' not in usedColor:
+		ansColList.append('/')
+	if '*' not in usedColor:
+		ansColList.append('*')
+
+	print ansColList
+
+	return ansCol
 
 def search(arg_list, y, x, arg_color):
+	# for tmp in cmplist :      
+	# 	print tmp
 
-	for tmp in cmplist :      
-		print tmp
-
-	print "------------------------"
+	# print "------------------------"
 	cmplist[y][x] = arg_color
 	poslist.append([y,x])
 	if y > 0 and cmplist[y-1][x] == ' ' and arg_list[y][x] == arg_list[y-1][x]:
@@ -58,34 +71,53 @@ def search(arg_list, y, x, arg_color):
 	if x > 0 and cmplist[y][x-1] == ' ' and arg_list[y][x] == arg_list[y][x-1]:
 		search(arg_list, y, x-1, arg_color)
 
+# input
+inputlist = []
+# output
+cmplist = []
+# position list
+poslist = []
+# position list list
+allposlist = []
+
 for line in open('data.txt', 'r'):
 	line = line.rstrip()
-	mylist.append(str(line))
+	inputlist.append(str(line))
 	#initialize output list
 	cmplist.append(map(str, str(' ' * len(line))))
 
-for line in open('color.txt', 'r'):
-	line = line.rstrip()
-	colorlist.append(map(str, str(line)))
+# for line in open('color.txt', 'r'):
+# 	line = line.rstrip()
+# 	colorlist.append(map(str, str(line)))
 
-# input
-for x in mylist :      
+
+for x in inputlist :      
 	print x;
 
-# for y in range(0, len(mylist)):
-# 	for x in range(0, len(mylist[0])):
-# 		if cmplist[y][x] == ' ':
-# 			posList = search(mylist, y, x, '*')	
-#			myColor = getColor(cmplist, posList)
-#			setColor(posList, myColor)
+for y in range(0, len(inputlist)):
+	for x in range(0, len(inputlist[0])):
+		if cmplist[y][x] == ' ':
+			poslist = []
+			allposlist.append(poslist)
+			search(inputlist, y, x, '@')	
+			myColor = getAvailableColor(cmplist, poslist)
+			#if myColor == '@'
+				# BackTrace!
+			setColor(poslist, myColor, cmplist)
+			#complete = checkComplete()
 
-search(mylist, 2, 5, '@')
+#search(mylist, 4, 7, '@')
+
+#myColor = getAvailableColor(colorlist, poslist)
+#setColor(poslist, myColor)
+
 # output
 for x in cmplist :      
 	print x;
 
-myColor = getAvailableColor(colorlist, poslist)
-setColor(poslist, myColor)
+# output
+for x in allposlist :      
+	print x;
 #print myColor
 #print checkChar(colorlist, 0, 0)
 
